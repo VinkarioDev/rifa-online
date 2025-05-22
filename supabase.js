2739a77b1a316679912e153aeebdc3bcecd73cb9
VinkarioDev <rogeriobuenodev@gmail.com>
2025-05-22 16:35:36 -0300

    INCLUIR A ANOM KEY COMPLETA

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://dqwlhtbxwndcjezfvypw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // sua chave inteira
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxd2xodGJ4d25kY2plemZ2eXB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MzgwMzcsImV4cCI6MjA2MzUxNDAzN30.oytkaqwB6EcUbuLhg6iI_TYVKz4jHWCqch9HyZ43UcM'; // sua chave inteira
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
